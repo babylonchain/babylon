3604a58c6dd6ae49da891c5cdfe2eb0f9a2e2a98
Vitalis Salis <vitsalis@gmail.com>
2022-06-10 17:22:41 +0200

    Remove third party directory and retrieve from cosmos

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,22 +2,20 @@
 
 set -eo pipefail
 
-protoc_gen_gocosmos() {
-  if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
-    echo -e "\tPlease run this command from somewhere inside the cosmos-sdk folder."
-    return 1
-  fi
+# get protoc executions
+go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@latest 2>/dev/null
+# get cosmos sdk from github
+go get github.com/cosmos/cosmos-sdk 2>/dev/null
 
-  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@latest 2>/dev/null
-}
-
-protoc_gen_gocosmos
+# Get the path of the cosmos-sdk repo from go/pkg/mod
+cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
 
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   buf protoc \
     -I "proto" \
-    -I "third_party/proto" \
+    -I "$cosmos_sdk_dir/third_party/proto" \
+    -I "$cosmos_sdk_dir/proto" \
     --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
     --grpc-gateway_out=logtostderr=true,allow_colon_final_segments=true:. \
