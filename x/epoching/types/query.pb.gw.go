a332b9aca6eb8a1b930ffff576bb7b14fc70f1fd
Runchao Han <runchao.han@monash.edu>
2022-06-17 12:04:56 +1000

    pagination param in QueryEpochMsgsRequest

--- x/epoching/types/query.pb.gw.go
+++ x/epoching/types/query.pb.gw.go
@@ -69,6 +69,10 @@ func local_request_Query_CurrentEpoch_0(ctx context.Context, marshaler runtime.M
 
 }
 
+var (
+	filter_Query_EpochMsgs_0 = &utilities.DoubleArray{Encoding: map[string]int{"epoch_num": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
 func request_Query_EpochMsgs_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryEpochMsgsRequest
 	var metadata runtime.ServerMetadata
@@ -89,6 +93,13 @@ func request_Query_EpochMsgs_0(ctx context.Context, marshaler runtime.Marshaler,
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "epoch_num", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_EpochMsgs_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.EpochMsgs(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -116,6 +127,13 @@ func local_request_Query_EpochMsgs_0(ctx context.Context, marshaler runtime.Mars
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "epoch_num", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_EpochMsgs_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.EpochMsgs(ctx, &protoReq)
