e1e4a6c043bec9d03b243cad06a155633f0dcf0b
KonradStaniec <konrad.staniec@gmail.com>
2022-06-20 17:31:54 +0200

    BM-6: Initial proposal for btc checkpoint message (#8)
    * Initial proposal for btc checkpoint message

--- x/btccheckpoint/keeper/msg_server.go
+++ x/btccheckpoint/keeper/msg_server.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"context"
+
 	"github.com/babylonchain/babylon/x/btccheckpoint/types"
 )
 
@@ -12,6 +14,11 @@ type msgServer struct {
 // for the provided Keeper.
 func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 	return &msgServer{keeper}
+}
+
+func (m msgServer) InsertBTCSpvProof(ctx context.Context, req *types.InsertBTCSpvProofRequest) (*types.InsertBTCSpvProofResponse, error) {
+	//TODO implement me
+	panic("implement me")
 }
 
 var _ types.MsgServer = msgServer{}
