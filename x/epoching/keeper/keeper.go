da7b642bc8bd51b393a1104a011502a370ce4de0
Runchao Han <runchao.han@monash.edu>
2022-06-21 17:12:28 +1000

    init

--- x/epoching/keeper/keeper.go
+++ x/epoching/keeper/keeper.go
@@ -12,6 +12,7 @@ import (
 
 const (
 	DefaultEpochNumber = 0
+	DefaultQueueLength = 0
 )
 
 type (
@@ -59,17 +60,6 @@ func (k *Keeper) SetHooks(eh types.EpochingHooks) *Keeper {
 	return k
 }
 
-// SetEpochNumber sets epoch number
-func (k Keeper) SetEpochNumber(ctx sdk.Context, epochNumber sdk.Uint) error {
-	store := ctx.KVStore(k.storeKey)
-	epochNumberBytes, err := epochNumber.Marshal()
-	if err != nil {
-		return err
-	}
-	store.Set(types.EpochNumberKey, epochNumberBytes)
-	return nil
-}
-
 // GetEpochNumber fetches epoch number
 func (k Keeper) GetEpochNumber(ctx sdk.Context) (sdk.Uint, error) {
 	store := ctx.KVStore(k.storeKey)
@@ -84,12 +74,103 @@ func (k Keeper) GetEpochNumber(ctx sdk.Context) (sdk.Uint, error) {
 	return epochNumber, err
 }
 
+// SetEpochNumber sets epoch number
+func (k Keeper) SetEpochNumber(ctx sdk.Context, epochNumber sdk.Uint) error {
+	store := ctx.KVStore(k.storeKey)
+
+	epochNumberBytes, err := epochNumber.Marshal()
+	if err != nil {
+		return err
+	}
+
+	store.Set(types.EpochNumberKey, epochNumberBytes)
+
+	return nil
+}
+
+func (k Keeper) IncrementEpochNumber(ctx sdk.Context) error {
+	epochNumber, err := k.GetEpochNumber(ctx)
+	if err != nil {
+		return err
+	}
+	incrementedEpochNumber := epochNumber.AddUint64(1)
+	return k.SetQueueLength(ctx, incrementedEpochNumber)
+}
+
+func (k Keeper) GetQueueLength(ctx sdk.Context) (sdk.Uint, error) {
+	store := ctx.KVStore(k.storeKey)
+
+	bz := store.Get(types.QueueLengthKey)
+	if bz == nil {
+		return sdk.NewUint(uint64(DefaultQueueLength)), nil
+	}
+	var queueLen sdk.Uint
+	err := queueLen.Unmarshal(bz)
+
+	return queueLen, err
+}
+
+func (k Keeper) SetQueueLength(ctx sdk.Context, queueLen sdk.Uint) error {
+	store := ctx.KVStore(k.storeKey)
+
+	queueLenBytes, err := queueLen.Marshal()
+	if err != nil {
+		return err
+	}
+
+	store.Set(types.EpochNumberKey, queueLenBytes)
+
+	return nil
+}
+
+func (k Keeper) IncrementQueueLength(ctx sdk.Context) error {
+	queueLen, err := k.GetQueueLength(ctx)
+	if err != nil {
+		return err
+	}
+	incrementedQueueLen := queueLen.AddUint64(1)
+	return k.SetQueueLength(ctx, incrementedQueueLen)
+}
+
 // GetEpochMsgs returns the set of messages queued of the current epoch
-func (k Keeper) GetEpochMsgs(ctx sdk.Context) []types.QueuedMessage {
-	panic("TODO: unimplemented")
+func (k Keeper) GetEpochMsgs(ctx sdk.Context) ([]types.QueuedMessage, error) {
+	queuedMsgs := []types.QueuedMessage{}
+
+	store := ctx.KVStore(k.storeKey)
+	iterator := sdk.KVStorePrefixIterator(ctx.KVStore(k.storeKey), types.QueuedMsgKey)
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		key := iterator.Key()
+		queuedMsgBytes := store.Get(key)
+		var queuedMsg types.QueuedMessage
+		if err := k.cdc.Unmarshal(queuedMsgBytes, &queuedMsg); err != nil {
+			return nil, err
+		}
+		queuedMsgs = append(queuedMsgs, queuedMsg)
+	}
+
+	return queuedMsgs, nil
 }
 
 // EnqueueMsg enqueues a message to the queue of the current epoch
 func (k Keeper) EnqueueMsg(ctx sdk.Context, msg types.QueuedMessage) error {
-	panic("TODO: unimplemented")
+	store := ctx.KVStore(k.storeKey)
+
+	queueLen, err := k.GetQueueLength(ctx)
+	if err != nil {
+		return err
+	}
+	queueLenBytes, err := queueLen.Marshal()
+	if err != nil {
+		return err
+	}
+
+	msgBytes, err := k.cdc.Marshal(&msg)
+	if err != nil {
+		return err
+	}
+	store.Set(append(types.QueuedMsgKey, queueLenBytes...), msgBytes)
+
+	return k.IncrementQueueLength(ctx)
 }
--- x/epoching/types/keys.go
+++ x/epoching/types/keys.go
@@ -17,10 +17,10 @@ const (
 	MemStoreKey = "mem_epoching"
 )
 
-// QueuedMsgKey is the key prefix for a queued message
 var (
-	EpochNumberKey = []byte{0x11}
-	QueuedMsgKey   = []byte{0x012}
+	EpochNumberKey = []byte{0x11} // key prefix for the epoch number
+	QueueLengthKey = []byte{0x12} // key prefix for the queue length
+	QueuedMsgKey   = []byte{0x13} // key prefix for a queued message
 )
 
 func KeyPrefix(p string) []byte {
