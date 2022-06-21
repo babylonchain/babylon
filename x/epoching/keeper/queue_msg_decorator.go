c9ec64e29d37b5755672c90cee6891f823fa189f
Runchao Han <runchao.han@monash.edu>
2022-06-21 14:59:04 +1000

    init

--- x/epoching/keeper/queue_msg_decorator.go
+++ x/epoching/keeper/queue_msg_decorator.go
@@ -1,7 +1,10 @@
 package keeper
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 // DropValidatorMsgDecorator defines an AnteHandler decorator that rejects all messages that might change the validator set.
@@ -18,8 +21,14 @@ func NewDropValidatorMsgDecorator() *DropValidatorMsgDecorator {
 // - MsgDelegate
 // - MsgUndelegate
 // - MsgBeginRedelegate
+// TODO: after we bump to Cosmos SDK v0.46, add MsgCancelUnbondingDelegation
 func (qmd DropValidatorMsgDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
-	panic("TODO: unimplemented")
+	for _, msg := range tx.GetMsgs() {
+		switch msg.(type) {
+		case *stakingtypes.MsgCreateValidator, *stakingtypes.MsgDelegate, *stakingtypes.MsgUndelegate, *stakingtypes.MsgBeginRedelegate:
+			return ctx, fmt.Errorf("intercepted some {MsgCreateValidator, MsgDelegate, MsgUndelegate, MsgBeginRedelegate} messages")
+		}
+	}
 
-	// return next(ctx, tx, simulate)
+	return next(ctx, tx, simulate)
 }
