aa07c4091789e18e1d41f81758562c299de85f6b
Runchao Han <runchao.han@monash.edu>
2022-06-24 11:04:54 +1000

    epoching: keeper functions, queries, and testing infra (#19)

--- x/epoching/keeper/grpc_query_test.go
+++ x/epoching/keeper/grpc_query_test.go
@@ -1,1 +1,22 @@
 package keeper_test
+
+import (
+	"testing"
+
+	testkeeper "github.com/babylonchain/babylon/testutil/keeper"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/babylonchain/babylon/x/epoching/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParamsQuery(t *testing.T) {
+	keeper, ctx := testkeeper.EpochingKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+	params := types.DefaultParams()
+	keeper.SetParams(ctx, params)
+
+	response, err := keeper.Params(wctx, &types.QueryParamsRequest{})
+	require.NoError(t, err)
+	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
+}
