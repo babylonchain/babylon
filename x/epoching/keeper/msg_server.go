602ffee245db6474bbb0bfab245b8f6b302a6e98
Runchao Han <runchao.han@monash.edu>
2022-06-28 17:21:08 +1000

    msgserver

--- x/epoching/keeper/msg_server.go
+++ x/epoching/keeper/msg_server.go
@@ -3,32 +3,79 @@ package keeper
 import (
 	"context"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/babylonchain/babylon/x/epoching/types"
 )
 
 type msgServer struct {
-	k Keeper
+	Keeper
 }
 
 // NewMsgServerImpl returns an implementation of the MsgServer interface
 // for the provided Keeper.
 func NewMsgServerImpl(keeper Keeper) types.MsgServer {
-	return &msgServer{k: keeper}
+	return &msgServer{Keeper: keeper}
 }
 
 var _ types.MsgServer = msgServer{}
 
 // WrappedDelegate handles the MsgWrappedDelegate request
 func (k msgServer) WrappedDelegate(goCtx context.Context, msg *types.MsgWrappedDelegate) (*types.MsgWrappedDelegateResponse, error) {
-	panic("TODO: unimplemented")
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// wrapped -> unwrapped -> QueuedMessage
+	queuedMsg := types.QueuedMessage{
+		MsgId: msg.GetSignBytes(), // TODO: not sure if this can be a good MsgId or not
+		Msg: &types.QueuedMessage_MsgDelegate{
+			MsgDelegate: msg.Msg,
+		},
+	}
+
+	// enqueue msg
+	if err := k.EnqueueMsg(ctx, queuedMsg); err != nil {
+		return nil, err
+	}
+
+	return &types.MsgWrappedDelegateResponse{}, nil
 }
 
 // WrappedUndelegate handles the MsgWrappedUndelegate request
 func (k msgServer) WrappedUndelegate(goCtx context.Context, msg *types.MsgWrappedUndelegate) (*types.MsgWrappedUndelegateResponse, error) {
-	panic("TODO: unimplemented")
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// wrapped -> unwrapped -> QueuedMessage
+	queuedMsg := types.QueuedMessage{
+		MsgId: msg.GetSignBytes(), // TODO: not sure if this can be a good MsgId or not
+		Msg: &types.QueuedMessage_MsgUndelegate{
+			MsgUndelegate: msg.Msg,
+		},
+	}
+
+	// enqueue msg
+	if err := k.EnqueueMsg(ctx, queuedMsg); err != nil {
+		return nil, err
+	}
+
+	return &types.MsgWrappedUndelegateResponse{}, nil
 }
 
 // WrappedBeginRedelegate handles the MsgWrappedBeginRedelegate request
 func (k msgServer) WrappedBeginRedelegate(goCtx context.Context, msg *types.MsgWrappedBeginRedelegate) (*types.MsgWrappedBeginRedelegateResponse, error) {
-	panic("TODO: unimplemented")
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// wrapped -> unwrapped -> QueuedMessage
+	queuedMsg := types.QueuedMessage{
+		MsgId: msg.GetSignBytes(), // TODO: not sure if this can be a good MsgId or not
+		Msg: &types.QueuedMessage_MsgBeginRedelegate{
+			MsgBeginRedelegate: msg.Msg,
+		},
+	}
+
+	// enqueue msg
+	if err := k.EnqueueMsg(ctx, queuedMsg); err != nil {
+		return nil, err
+	}
+
+	return &types.MsgWrappedBeginRedelegateResponse{}, nil
 }
