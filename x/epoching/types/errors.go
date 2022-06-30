ac79e527ad248fba8a16f9d7bb6ee10d09d82a37
Runchao Han <runchao.han@monash.edu>
2022-06-30 22:13:18 +1000

    minor

--- x/epoching/types/errors.go
+++ x/epoching/types/errors.go
@@ -9,5 +9,5 @@ import (
 // x/epoching module sentinel errors
 var (
 	ErrInvalidMsgType     = sdkerrors.Register(ModuleName, 1, "invalid message type in {MsgCreateValidator, MsgDelegate, MsgUndelegate, MsgBeginRedelegate} messages. use wrapped versions instead")
-	ErrUnknownEpochNumber = sdkerrors.Register(ModuleName, 1, "the epoch number is not known in DB")
+	ErrUnknownEpochNumber = sdkerrors.Register(ModuleName, 2, "the epoch number is not known in DB")
 )
