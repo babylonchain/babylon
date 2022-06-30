01b74552f7872f07d2b05a89aa1a67b7f31de7ca
Fangyu Gai <greferry@gmail.com>
2022-06-30 11:44:35 +0800

    feat: add hooks, events, and error types to the checkpointing module (#33)

--- x/checkpointing/keeper/hooks.go
+++ x/checkpointing/keeper/hooks.go
@@ -1,1 +1,25 @@
 package keeper
+
+import (
+	"github.com/babylonchain/babylon/x/checkpointing/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// Implements CheckpointingHooks interface
+var _ types.CheckpointingHooks = Keeper{}
+
+// AfterBlsKeyRegistered - call hook if registered
+func (k Keeper) AfterBlsKeyRegistered(ctx sdk.Context, valAddr sdk.ValAddress) error {
+	if k.hooks != nil {
+		return k.hooks.AfterBlsKeyRegistered(ctx, valAddr)
+	}
+	return nil
+}
+
+// AfterRawCheckpointConfirmed - call hook if registered
+func (k Keeper) AfterRawCheckpointConfirmed(ctx sdk.Context, epoch sdk.Uint) error {
+	if k.hooks != nil {
+		return k.hooks.AfterRawCheckpointConfirmed(ctx, epoch)
+	}
+	return nil
+}
