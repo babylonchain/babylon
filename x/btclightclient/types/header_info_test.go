1252bf84896ad69f79e2fc9ab684d9cf997be968
Vitalis Salis <vitsalis@gmail.com>
2022-06-30 13:40:12 +0300

    Add HeaderInfo tests

---
+++ x/btclightclient/types/header_info_test.go
@@ -0,0 +1,65 @@
+package types_test
+
+import (
+	"bytes"
+	bbl "github.com/babylonchain/babylon/types"
+	"github.com/babylonchain/babylon/x/btclightclient/types"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/wire"
+	"testing"
+	"time"
+)
+
+func FuzzNewHeaderInfo(f *testing.F) {
+	defaultHeader, _ := bbl.NewBTCHeaderBytesFromHex(types.DefaultBaseHeaderHex)
+	btcdHeader, _ := defaultHeader.ToBlockHeader()
+	f.Add(
+		btcdHeader.Version,
+		btcdHeader.Bits,
+		btcdHeader.Nonce,
+		btcdHeader.Timestamp.Unix(),
+		btcdHeader.PrevBlock.String(),
+		btcdHeader.MerkleRoot.String())
+
+	f.Fuzz(func(t *testing.T, version int32, bits uint32, nonce uint32,
+		timeInt int64, prevBlockStr string, merkleRootStr string) {
+
+		prevBlock, err := chainhash.NewHashFromStr(prevBlockStr)
+		if err != nil {
+			t.Skip()
+		}
+		merkleRoot, err := chainhash.NewHashFromStr(merkleRootStr)
+		if err != nil {
+			t.Skip()
+		}
+		time := time.Unix(timeInt, 0)
+
+		header := wire.BlockHeader{
+			Version:    version,
+			Bits:       bits,
+			Nonce:      nonce,
+			PrevBlock:  *prevBlock,
+			MerkleRoot: *merkleRoot,
+			Timestamp:  time,
+		}
+
+		headerInfo := types.NewHeaderInfo(&header)
+
+		if headerInfo == nil {
+			t.Errorf("returned object is nil")
+		}
+
+		gotHeader := *headerInfo.Header
+		expectedHeader := bbl.NewBTCHeaderBytesFromBlockHeader(&header)
+		if bytes.Compare(expectedHeader, gotHeader) != 0 {
+			t.Errorf("Expected header %s got %s", expectedHeader, gotHeader)
+		}
+
+		gotHash := *headerInfo.Hash
+		blockHash := header.BlockHash()
+		expectedHash := bbl.NewBTCHeaderHashBytesFromChainhash(&blockHash)
+		if bytes.Compare(expectedHash, gotHash) != 0 {
+			t.Errorf("Expected header hash %s got %s", expectedHash, gotHash)
+		}
+	})
+}
