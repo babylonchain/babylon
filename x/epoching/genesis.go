ffdee5383c6cb084e7520c2c65631598a1f41caf
Runchao Han <runchao.han@monash.edu>
2022-06-30 15:47:40 +1000

    minor

--- x/epoching/genesis.go
+++ x/epoching/genesis.go
@@ -1,15 +1,15 @@
 package epoching
 
 import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/babylonchain/babylon/x/epoching/keeper"
 	"github.com/babylonchain/babylon/x/epoching/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // InitGenesis initializes the capability module's state from a provided genesis
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
-    // this line is used by starport scaffolding # genesis/module/init
+	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
 }
 
@@ -18,7 +18,7 @@ func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
 
-    // this line is used by starport scaffolding # genesis/module/export
+	// this line is used by starport scaffolding # genesis/module/export
 
-    return genesis
+	return genesis
 }
--- x/epoching/types/genesis_test.go
+++ x/epoching/types/genesis_test.go
@@ -45,7 +45,6 @@ func TestGenesisState_Validate(t *testing.T) {
 			},
 			valid: true,
 		},
-		// this line is used by starport scaffolding # types/genesis/testcase
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
 			err := tc.genState.Validate()
