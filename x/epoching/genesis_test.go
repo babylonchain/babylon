56d6f89e81ec823d9afc9b8ed49a370e3e8d3222
Runchao Han <runchao.han@monash.edu>
2022-07-01 16:53:37 +1000

    test

--- x/epoching/genesis_test.go
+++ x/epoching/genesis_test.go
@@ -1,1 +1,21 @@
 package epoching_test
+
+import (
+	"testing"
+
+	"github.com/babylonchain/babylon/x/epoching"
+	"github.com/stretchr/testify/require"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	simapp "github.com/babylonchain/babylon/app"
+	"github.com/babylonchain/babylon/x/epoching/types"
+)
+
+func TestExportGenesis(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	app.EpochingKeeper.SetParams(ctx, types.DefaultParams())
+	genesisState := epoching.ExportGenesis(ctx, app.EpochingKeeper)
+	require.Equal(t, genesisState.Params, types.DefaultParams())
+}
