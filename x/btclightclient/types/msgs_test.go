054d55f154d5271c57b2d5aaa658df0efb25c327
Vitalis Salis <vitsalis@gmail.com>
2022-07-05 17:18:19 +0300

    Refactor msgs test

--- x/btclightclient/types/msgs_test.go
+++ x/btclightclient/types/msgs_test.go
@@ -5,82 +5,93 @@ import (
 	bbl "github.com/babylonchain/babylon/types"
 	"github.com/babylonchain/babylon/x/btclightclient/types"
 	"github.com/btcsuite/btcd/blockchain"
-	"github.com/btcsuite/btcd/btcutil"
-	btcchaincfg "github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/wire"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"math/big"
+	"math/rand"
 	"testing"
 )
 
 func FuzzMsgInsertHeader(f *testing.F) {
 	addressStr := "from________________"
 	addressBytes := []byte(addressStr)
+	maxDifficulty, _ := new(big.Int).SetString("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 16)
 
-	f.Add(addressBytes, types.DefaultBaseHeaderHex)
-	f.Fuzz(func(t *testing.T, addressBytes []byte, headerHex string) {
-		msgCreationShouldFail := false
-		validateShouldFail := false
+	f.Add(addressBytes, types.DefaultBaseHeaderHex, int64(17))
+	f.Fuzz(func(t *testing.T, addressBytes []byte, headerHex string, seed int64) {
+		seedInput := types.DefaultBaseHeaderHex == headerHex
+		errorKind := 0
 
+		// Populate proper data
+		rand.Seed(seed)
+		if len(addressBytes) == 0 || len(addressBytes) >= 256 {
+			addressBytes = genRandomByteArray(1 + uint64(rand.Intn(257)))
+		}
+		if !validHex(headerHex, bbl.BTCHeaderLen) {
+			headerHex = genRandomHexStr(bbl.BTCHeaderLen)
+		}
+
+		// Even if the data has the proper structure, some checks still fail
 		var signer sdk.AccAddress
 		err := signer.Unmarshal(addressBytes)
 		if err != nil {
 			// Invalid address
 			t.Skip()
 		}
-
-		header, err := bbl.NewBTCHeaderBytesFromHex(headerHex)
+		tmpHeader, err := bbl.NewBTCHeaderBytesFromHex(headerHex)
 		if err != nil {
-			// Invalid header, later this should fail
-			msgCreationShouldFail = true
+			t.Skip()
 		}
-
-		btcdHeader, err := header.ToBlockHeader()
+		tmpBtcdHeader, err := tmpHeader.ToBlockHeader()
 		if err != nil {
 			// Cannot be converted to a block header
 			t.Skip()
 		}
 
-		// Create a block to check proof of work
-		msgBlock := &wire.MsgBlock{Header: *btcdHeader}
-		block := btcutil.NewBlock(msgBlock)
-		// TODO: get the parameter from a configuration file
-		if blockchain.CheckProofOfWork(block, btcchaincfg.MainNetParams.PowLimit) != nil {
-			// There's an invalid work parameter
-			validateShouldFail = true
+		// Change the btcd header and convert back to hex
+		if !seedInput {
+			errorKind = rand.Intn(3)
+			switch errorKind {
+			case 0:
+				// Valid input
+				// Set the work bits to the pow limit
+				tmpBtcdHeader.Bits = blockchain.BigToCompact(maxDifficulty)
+			case 1:
+				// Zero PoW
+				tmpBtcdHeader.Bits = blockchain.BigToCompact(big.NewInt(0))
+			case 2:
+				// Negative PoW
+				tmpBtcdHeader.Bits = blockchain.BigToCompact(big.NewInt(-1))
+			}
+		}
+		header := bbl.NewBTCHeaderBytesFromBlockHeader(tmpBtcdHeader)
+		headerHex, err = header.MarshalHex()
+		if err != nil {
+			// btcd header can't be marshalled to hex
+			t.Skip()
 		}
 
-		if len(addressBytes) == 0 || len(addressBytes) >= 256 {
-			// signers should be between 1-256 bytes
-			validateShouldFail = true
-		}
-
+		// Check the message creation
 		msgInsertHeader, err := types.NewMsgInsertHeader(signer, headerHex)
 		if err != nil {
-			if msgCreationShouldFail {
-				// All good, this should have failed
-				t.Skip()
-			}
 			t.Errorf("Valid parameters led to error")
 		}
-
 		if msgInsertHeader == nil {
 			t.Errorf("nil returned")
 		}
-
 		if msgInsertHeader.Header == nil {
 			t.Errorf("nil header")
 		}
-
 		if bytes.Compare(header, *(msgInsertHeader.Header)) != 0 {
 			t.Errorf("Expected header bytes %s got %s", header, *(msgInsertHeader.Header))
 		}
 
-		err = msgInsertHeader.ValidateBasic()
-		if err != nil {
-			if validateShouldFail {
-				t.Skip()
-			}
-			t.Errorf("Validation for valid message failed with err %s", err)
+		// Validate the message
+		err = msgInsertHeader.ValidateHeader(maxDifficulty)
+		if err != nil && errorKind == 0 {
+			t.Errorf("Valid message %s failed with %s", headerHex, err)
+		}
+		if err == nil && errorKind != 0 {
+			t.Errorf("Invalid message did not fail")
 		}
 	})
 
