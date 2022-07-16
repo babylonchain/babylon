09b04f95cffa6bafcd19025e8c3d0361475bc8ac
KonradStaniec <konrad.staniec@gmail.com>
2022-07-16 08:05:57 +0200

    Btccheckpoint oracle schema and processing (#43)
    * Btccheckpoint oracle schema

--- x/btccheckpoint/types/errors.go
+++ x/btccheckpoint/types/errors.go
@@ -8,5 +8,9 @@ import (
 
 // x/btccheckpoint module sentinel errors
 var (
-	ErrInvalidCheckpointProof = sdkerrors.Register(ModuleName, 1100, "Invalid checkpoint proof")
+	ErrInvalidCheckpointProof            = sdkerrors.Register(ModuleName, 1100, "Invalid checkpoint proof")
+	ErrDuplicatedSubmission              = sdkerrors.Register(ModuleName, 1101, "Duplicated submission")
+	ErrNoCheckpointsForPreviousEpoch     = sdkerrors.Register(ModuleName, 1102, "No checkpoints for previous epoch")
+	ErrProvidedHeaderFromDifferentForks  = sdkerrors.Register(ModuleName, 1103, "Proof header from different forks")
+	ErrProvidedHeaderDoesNotHaveAncestor = sdkerrors.Register(ModuleName, 1104, "Proof header does not have ancestor in previous epoch")
 )
