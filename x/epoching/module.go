56944dd18950f60e0aa9e97f38301ef2d859742d
Jack Liu <toliujiayi@gmail.com>
2022-08-09 13:49:37 -0700

    enable gRPC gateway routes for rest queries

--- x/epoching/module.go
+++ x/epoching/module.go
@@ -1,6 +1,7 @@
 package epoching
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 
@@ -79,7 +80,7 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	// this line is used by starport scaffolding # 2
+	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
 }
 
 // GetTxCmd returns the capability module's root tx command.
