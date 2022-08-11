7b982bf71420ad9b749e7c76bce92b73beb76147
Fangyu Gai <greferry@gmail.com>
2022-08-11 20:02:38 +0800

    added checkpoint query with status (#89)

--- x/checkpointing/types/bls_key.pb.go
+++ x/checkpointing/types/bls_key.pb.go
@@ -337,7 +337,7 @@ func (m *BlsKey) Unmarshal(dAtA []byte) error {
 			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field PoP", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Pop", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
