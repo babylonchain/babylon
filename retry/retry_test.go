1a41603a1be05081e6bd7204c9d3698bfb77c101
Gurjot <gurjotsyd@gmail.com>
2022-10-01 18:12:40 -0400

    add unit tests

--- retry/retry_test.go
+++ retry/retry_test.go
@@ -1,1 +1,21 @@
 package retry
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestUnrecoverableError(t *testing.T) {
+	err := Do(1*time.Second, 1*time.Minute, func() error {
+		return unrecoverableErrors[0]
+	})
+	require.Error(t, err)
+}
+
+func TestExpectedError(t *testing.T) {
+	err := Do(1*time.Second, 1*time.Minute, func() error {
+		return expectedErrors[0]
+	})
+	require.NoError(t, err)
+}
