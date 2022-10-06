e4b2407d398e0e341ff1db43dc724cfdc4ac21e5
Fangyu Gai <greferry@gmail.com>
2022-10-06 10:53:52 +0800

    improve cli

--- cmd/babylond/cmd/create_bls_key.go
+++ cmd/babylond/cmd/create_bls_key.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
 	"github.com/babylonchain/babylon/app"
+	appparams "github.com/babylonchain/babylon/app/params"
 	"github.com/babylonchain/babylon/crypto/bls12381"
 	"github.com/babylonchain/babylon/privval"
 	"github.com/cosmos/cosmos-sdk/client/flags"
@@ -11,33 +13,34 @@ import (
 	tmconfig "github.com/tendermint/tendermint/config"
 	tmos "github.com/tendermint/tendermint/libs/os"
 	"path/filepath"
-)
-
-var (
-	flagAccountAddress = "account-address"
+	"strings"
 )
 
 func createBlsKeyCmd() *cobra.Command {
+	bech32PrefixAccAddr := appparams.Bech32PrefixAccAddr
+
 	cmd := &cobra.Command{
 		Use:   "create-bls-key [account-address]",
+		Args:  cobra.ExactArgs(1),
 		Short: "Create a pair of BLS keys for a validator",
-		Long: `create-bls will create a pair of BLS keys that are used to
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`create-bls will create a pair of BLS keys that are used to
 send BLS signatures for checkpointing.
 
 BLS keys are stored along with other validator keys in priv_validator_key.json,
-which should exist before running the command.
+which should exist before running the command (via babylond init or babylond testnet).
 
 Example:
-	babylond create-bls-key --account-address bbn1f5tnl46mk4dfp4nx3n2vnrvyw2h2ydz6ykhk3r --home ./
-	`,
-		RunE: func(cmd *cobra.Command, _ []string) error {
-			homeDir, _ := cmd.Flags().GetString(flags.FlagHome)
-			accAddr, err := cmd.Flags().GetString(flagAccountAddress)
-			if err != nil {
-				return err
-			}
+$ babylond create-bls-key %s1f5tnl46mk4dfp4nx3n2vnrvyw2h2ydz6ykhk3r --home ./
+`,
+				bech32PrefixAccAddr,
+			),
+		),
 
-			addr, err := sdk.AccAddressFromBech32(accAddr)
+		RunE: func(cmd *cobra.Command, args []string) error {
+			homeDir, _ := cmd.Flags().GetString(flags.FlagHome)
+
+			addr, err := sdk.AccAddressFromBech32(args[0])
 			if err != nil {
 				return err
 			}
@@ -47,7 +50,6 @@ Example:
 	}
 
 	cmd.Flags().String(flags.FlagHome, app.DefaultNodeHome, "The node home directory")
-	cmd.Flags().String(flagAccountAddress, "", "The account address associated with the validator")
 
 	return cmd
 }
