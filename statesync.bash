7872407314f23e828988169753c33137f3c37253
Jacob Gadikian <jacobgadikian@gmail.com>
2022-11-25 19:09:22 +0700

    Update statesync.bash
    Co-authored-by: Vitalis Salis <VitSalis@gmail.com>

--- statesync.bash
+++ statesync.bash
@@ -6,7 +6,7 @@
 ## USAGE RUNDOWN
 # Not for use on live nodes
 # For use when testing.
-# Assumes that ~/.evmosd doesn't exist
+# Assumes that ~/.babylond doesn't exist
 # can be modified to suit your purposes if ~/.evmosd does already exist
 
 set -uxe
