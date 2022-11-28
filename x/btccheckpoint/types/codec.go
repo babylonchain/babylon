ef0fab91e1306fcbd263ae30b7267996c6d70034
Jacob Gadikian <jacobgadikian@gmail.com>
2022-11-28 18:11:38 +0700

    chore: Remove starport references (#225)
    Co-authored-by: Vitalis Salis <vitsalis@gmail.com>

--- x/btccheckpoint/types/codec.go
+++ x/btccheckpoint/types/codec.go
@@ -5,16 +5,13 @@ import (
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	// this line is used by starport scaffolding # 1
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
-	// this line is used by starport scaffolding # 2
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	// this line is used by starport scaffolding # 3
 
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgInsertBTCSpvProof{},
