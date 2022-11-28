54dab9adbb9df3b21dd6324e28d8545ea4c36173
Jacob Gadikian <jacobgadikian@gmail.com>
2022-11-28 17:47:17 +0700

    check perpare genesis error

--- x/btccheckpoint/handler.go
+++ x/btccheckpoint/handler.go
@@ -14,7 +14,6 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 	// this line is used by starport scaffolding # handler/msgServer
 
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
-		_ = ctx.WithEventManager(sdk.NewEventManager())
 
 		switch msg := msg.(type) {
 		// this line is used by starport scaffolding # 1
