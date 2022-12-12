d297ba5d122ba69f2190f9b02cf8e44eae8e630f
Runchao Han <elvisage941102@gmail.com>
2022-12-12 16:52:31 +1100

    init

--- x/zoneconcierge/keeper/proof_epoch_sealed.go
+++ x/zoneconcierge/keeper/proof_epoch_sealed.go
@@ -8,10 +8,23 @@ import (
 	checkpointingtypes "github.com/babylonchain/babylon/x/checkpointing/types"
 	epochingtypes "github.com/babylonchain/babylon/x/epoching/types"
 	"github.com/babylonchain/babylon/x/zoneconcierge/types"
+	"github.com/cosmos/cosmos-sdk/store/rootmulti"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	tmcrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
 	rpcclient "github.com/tendermint/tendermint/rpc/client"
 )
+
+func getEpochInfoKey(epochNumber uint64) []byte {
+	epochInfoKey := epochingtypes.EpochInfoKey
+	epochInfoKey = append(epochInfoKey, sdk.Uint64ToBigEndian(epochNumber)...)
+	return epochInfoKey
+}
+
+func getValSetKey(epochNumber uint64) []byte {
+	valSetKey := checkpointingtypes.ValidatorBlsKeySetPrefix
+	valSetKey = append(valSetKey, sdk.Uint64ToBigEndian(epochNumber)...)
+	return valSetKey
+}
 
 // queryStore queries a KV pair in the KVStore, where
 // - moduleStoreKey is the store key of a module, e.g., zctypes.StoreKey
@@ -21,16 +34,20 @@ import (
 // - value of this KV pair
 // - Merkle proof of this KV pair
 // - error
-func (k Keeper) queryStore(ctx sdk.Context, moduleStoreKey string, key []byte) ([]byte, []byte, *tmcrypto.ProofOps, error) {
+func (k Keeper) queryStore(moduleStoreKey string, key []byte, queryHeight int64) ([]byte, []byte, *tmcrypto.ProofOps, error) {
 	prefix := fmt.Sprintf("/store/%s/key", moduleStoreKey) // path of the entry in KVStore
 	opts := rpcclient.ABCIQueryOptions{
-		Height: ctx.BlockHeight(),
+		Height: queryHeight,
 		Prove:  true,
 	}
 	resp, err := k.tmClient.ABCIQueryWithOptions(context.Background(), prefix, key, opts)
 	return resp.Response.Key, resp.Response.Value, resp.Response.ProofOps, err
 }
 
+// ProveEpochSealed proves an epoch has been sealed, i.e.,
+// - the epoch's validator set has a valid multisig over the sealer header
+// - the epoch's validator set is committed to the sealer header's last_commit_hash
+// - the epoch's metadata is committed to the sealer header's last_commit_hash
 func (k Keeper) ProveEpochSealed(ctx sdk.Context, epochNumber uint64) (*types.ProofEpochSealed, error) {
 	var (
 		proof *types.ProofEpochSealed = &types.ProofEpochSealed{}
@@ -43,23 +60,33 @@ func (k Keeper) ProveEpochSealed(ctx sdk.Context, epochNumber uint64) (*types.Pr
 		return nil, err
 	}
 
+	// get sealer header and the query height
+	epoch, err := k.epochingKeeper.GetHistoricalEpoch(ctx, epochNumber)
+	if err != nil {
+		return nil, err
+	}
+	sealedHeight := epoch.SealerHeader.Height
+
 	// proof of inclusion for epoch metadata in sealer header
-	epochInfoKey := epochingtypes.EpochInfoKey
-	epochInfoKey = append(epochInfoKey, sdk.Uint64ToBigEndian(epochNumber)...)
-	_, _, proof.ProofEpochInfo, err = k.queryStore(ctx, epochingtypes.StoreKey, epochInfoKey)
+	epochInfoKey := getEpochInfoKey(epochNumber)
+	_, _, proof.ProofEpochInfo, err = k.queryStore(epochingtypes.StoreKey, epochInfoKey, sealedHeight)
 	if err != nil {
 		return nil, err
 	}
 
 	// proof of inclusion for validator set in sealer header
-	valSetKey := checkpointingtypes.ValidatorBlsKeySetPrefix
-	valSetKey = append(valSetKey, sdk.Uint64ToBigEndian(epochNumber)...)
-	_, _, proof.ProofEpochValSet, err = k.queryStore(ctx, epochingtypes.StoreKey, valSetKey)
+	valSetKey := getValSetKey(epochNumber)
+	_, _, proof.ProofEpochValSet, err = k.queryStore(epochingtypes.StoreKey, valSetKey, sealedHeight)
 	if err != nil {
 		return nil, err
 	}
 
 	return proof, nil
+}
+
+func verifyStore(root []byte, keyWithPrefix []byte, value []byte, proof *tmcrypto.ProofOps) error {
+	prt := rootmulti.DefaultProofRuntime()
+	return prt.VerifyValue(proof, root, string(keyWithPrefix), value)
 }
 
 // VerifyEpochSealed verifies that the given `epoch` is sealed by the `rawCkpt` by using the given `proof`
@@ -87,9 +114,6 @@ func VerifyEpochSealed(epoch *epochingtypes.Epoch, rawCkpt *checkpointingtypes.R
 	} else if proof.ValidateBasic(); err != nil {
 		return err
 	}
-
-	// TODO: Ensure The epoch medatata is committed to the app_hash of the sealer header
-	// TODO: Ensure The validator set is committed to the app_hash of the sealer header
 
 	// ensure epoch number is same in epoch and rawCkpt
 	if epoch.EpochNumber != rawCkpt.EpochNum {
@@ -128,6 +152,27 @@ func VerifyEpochSealed(epoch *epochingtypes.Epoch, rawCkpt *checkpointingtypes.R
 	}
 	if !ok {
 		return fmt.Errorf("BLS signature does not match the public key")
+	}
+
+	// get the Merkle root, i.e., the LastCommitHash of the sealer header
+	root := epoch.SealerHeader.LastCommitHash
+
+	// Ensure The epoch medatata is committed to the app_hash of the sealer header
+	epochBytes, err := epoch.Marshal()
+	if err != nil {
+		return err
+	}
+	if err := verifyStore(root, getEpochInfoKey(epoch.EpochNumber), epochBytes, proof.ProofEpochInfo); err != nil {
+		return err
+	}
+
+	// Ensure The validator set is committed to the app_hash of the sealer header
+	valSetBytes, err := valSet.Marshal()
+	if err != nil {
+		return err
+	}
+	if err := verifyStore(root, getValSetKey(epoch.EpochNumber), valSetBytes, proof.ProofEpochValSet); err != nil {
+		return err
 	}
 
 	return nil
