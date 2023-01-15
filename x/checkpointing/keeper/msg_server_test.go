b53005906f9e911367e1d12b32e9c486efa98ea8
Runchao Han <elvisage941102@gmail.com>
2023-01-16 10:44:22 +1100

    test

--- x/checkpointing/keeper/msg_server_test.go
+++ x/checkpointing/keeper/msg_server_test.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 	"testing"
 
+	"cosmossdk.io/math"
 	"github.com/babylonchain/babylon/app"
 	appparams "github.com/babylonchain/babylon/app/params"
 	"github.com/babylonchain/babylon/crypto/bls12381"
@@ -18,6 +19,82 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/crypto/ed25519"
 )
+
+// FuzzWrappedCreateValidator_InsufficientTokens tests adding new validators with zero voting power
+// It ensures that validators with zero voting power (i.e., with tokens fewer than sdk.DefaultPowerReduction)
+// are unbonded, thus are not included in the validator
+func FuzzWrappedCreateValidator_InsufficientTokens(f *testing.F) {
+	datagen.AddRandomSeedsToFuzzer(f, 4)
+
+	f.Fuzz(func(t *testing.T, seed int64) {
+		rand.Seed(seed)
+
+		// a genesis validator is generate for setup
+		helper := testepoching.NewHelper(t)
+		ek := helper.EpochingKeeper
+		ck := helper.App.CheckpointingKeeper
+		msgServer := checkpointingkeeper.NewMsgServerImpl(ck)
+
+		// BeginBlock of block 1, and thus entering epoch 1
+		ctx := helper.BeginBlock()
+		epoch := ek.GetEpoch(ctx)
+		require.Equal(t, uint64(1), epoch.EpochNumber)
+
+		n := rand.Intn(3) + 1
+		addrs := app.AddTestAddrs(helper.App, helper.Ctx, n, sdk.NewInt(100000000))
+
+		// add n new validators with zero voting power via MsgWrappedCreateValidator
+		wcvMsgs := make([]*types.MsgWrappedCreateValidator, n)
+		for i := 0; i < n; i++ {
+			msg, err := buildMsgWrappedCreateValidatorWithAmount(addrs[i], sdk.DefaultPowerReduction.SubRaw(1))
+			require.NoError(t, err)
+			wcvMsgs[i] = msg
+			_, err = msgServer.WrappedCreateValidator(ctx, msg)
+			require.NoError(t, err)
+			blsPK, err := ck.GetBlsPubKey(ctx, sdk.ValAddress(addrs[i]))
+			require.NoError(t, err)
+			require.True(t, msg.Key.Pubkey.Equal(blsPK))
+		}
+		require.Len(t, ek.GetCurrentEpochMsgs(ctx), n)
+
+		// EndBlock of block 1
+		ctx = helper.EndBlock()
+
+		// go to BeginBlock of block 11, and thus entering epoch 2
+		for i := uint64(0); i < ek.GetParams(ctx).EpochInterval; i++ {
+			ctx = helper.GenAndApplyEmptyBlock()
+		}
+		epoch = ek.GetEpoch(ctx)
+		require.Equal(t, uint64(2), epoch.EpochNumber)
+		// ensure epoch 2 has initialised an empty msg queue
+		require.Empty(t, ek.GetCurrentEpochMsgs(ctx))
+
+		// ensure the length of current validator set equals to 1
+		// since one genesis validator was added when setup
+		// the rest n validators have zero voting power and thus are ruled out
+		valSet = ck.GetValidatorSet(ctx, 2)
+		require.Equal(t, 1, len(valSet))
+
+		// ensure all validators (not just validators in the val set) have correct bond status
+		// - the 1st validator is bonded
+		// - all the rest are unbonded since they have zero voting power
+		iterator := helper.StakingKeeper.ValidatorsPowerStoreIterator(ctx)
+		defer iterator.Close()
+		count := 0
+		for ; iterator.Valid(); iterator.Next() {
+			valAddr := sdk.ValAddress(iterator.Value())
+			val, found := helper.StakingKeeper.GetValidator(ctx, valAddr)
+			require.True(t, found)
+			count++
+			if count == 1 {
+				require.Equal(t, stakingtypes.Bonded, val.Status)
+			} else {
+				require.Equal(t, stakingtypes.Unbonded, val.Status)
+			}
+		}
+		require.Equal(t, len(wcvMsgs)+1, count)
+	})
+}
 
 // FuzzWrappedCreateValidator tests adding new validators via
 // MsgWrappedCreateValidator, which first registers BLS pubkey
@@ -89,6 +166,33 @@ func FuzzWrappedCreateValidator(f *testing.F) {
 func buildMsgWrappedCreateValidator(addr sdk.AccAddress) (*types.MsgWrappedCreateValidator, error) {
 	tmValPrivkey := ed25519.GenPrivKey()
 	bondTokens := sdk.TokensFromConsensusPower(10, sdk.DefaultPowerReduction)
+	bondCoin := sdk.NewCoin(appparams.DefaultBondDenom, bondTokens)
+	description := stakingtypes.NewDescription("foo_moniker", "", "", "", "")
+	commission := stakingtypes.NewCommissionRates(sdk.ZeroDec(), sdk.ZeroDec(), sdk.ZeroDec())
+
+	pk, err := codec.FromTmPubKeyInterface(tmValPrivkey.PubKey())
+	if err != nil {
+		return nil, err
+	}
+
+	createValidatorMsg, err := stakingtypes.NewMsgCreateValidator(
+		sdk.ValAddress(addr), pk, bondCoin, description, commission, sdk.OneInt(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	blsPrivKey := bls12381.GenPrivKey()
+	pop, err := privval.BuildPoP(tmValPrivkey, blsPrivKey)
+	if err != nil {
+		return nil, err
+	}
+	blsPubKey := blsPrivKey.PubKey()
+
+	return types.NewMsgWrappedCreateValidator(createValidatorMsg, &blsPubKey, pop)
+}
+
+func buildMsgWrappedCreateValidatorWithAmount(addr sdk.AccAddress, bondTokens math.Int) (*types.MsgWrappedCreateValidator, error) {
+	tmValPrivkey := ed25519.GenPrivKey()
 	bondCoin := sdk.NewCoin(appparams.DefaultBondDenom, bondTokens)
 	description := stakingtypes.NewDescription("foo_moniker", "", "", "", "")
 	commission := stakingtypes.NewCommissionRates(sdk.ZeroDec(), sdk.ZeroDec(), sdk.ZeroDec())
