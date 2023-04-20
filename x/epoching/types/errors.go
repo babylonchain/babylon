c1ac556e25fa4fa99d07e8e5f8c358be87295c64
Vitalis Salis <VitSalis@gmail.com>
2023-04-20 09:34:50 +0300

    chore: Use more descriptive error message for using wrapped transactions (#355)

--- x/epoching/types/errors.go
+++ x/epoching/types/errors.go
@@ -6,12 +6,13 @@ import (
 	errorsmod "cosmossdk.io/errors"
 )
 
-// x/epoching module sentinel errors
 var (
-	ErrUnwrappedMsgType          = errorsmod.Register(ModuleName, 1, "invalid message type in {MsgCreateValidator, MsgDelegate, MsgUndelegate, MsgBeginRedelegate} messages. use wrapped versions instead")
+	ErrUnwrappedMsgType = errorsmod.Register(ModuleName, 1, `
+										invalid message type in {MsgCreateValidator, MsgDelegate, MsgUndelegate, MsgBeginRedelegate}
+										messages. For creating a validator use the wrapped version under 'tx checkpointing create-validator'
+										and for the other messages use the wrapped versions under 'tx epoching {delegate,undelegate,redelegate}'`)
 	ErrInvalidQueuedMessageType  = errorsmod.Register(ModuleName, 2, "invalid message type of a QueuedMessage")
 	ErrUnknownEpochNumber        = errorsmod.Register(ModuleName, 3, "the epoch number is not known in DB")
-	ErrUnknownQueueLen           = errorsmod.Register(ModuleName, 4, "the msg queue length is not known in DB")
 	ErrUnknownSlashedVotingPower = errorsmod.Register(ModuleName, 5, "the slashed voting power is not known in DB. Maybe the epoch has been checkpointed?")
 	ErrUnknownValidator          = errorsmod.Register(ModuleName, 6, "the validator is not known in the validator set.")
 	ErrUnknownTotalVotingPower   = errorsmod.Register(ModuleName, 7, "the total voting power is not known in DB.")
