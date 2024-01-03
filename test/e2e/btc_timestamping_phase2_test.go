cd571ee996b5c39af7ecc2f032dbda94f36be906
Mauro Lacy <mauro@lacy.com.es>
2024-01-03 17:01:25 +0100

    Add (failing) next IBC packet sequence check

--- test/e2e/btc_timestamping_phase2_test.go
+++ test/e2e/btc_timestamping_phase2_test.go
@@ -1,12 +1,38 @@
 package e2e
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/babylonchain/babylon/test/e2e/configurer"
 	"github.com/babylonchain/babylon/test/e2e/initialization"
 	ct "github.com/babylonchain/babylon/x/checkpointing/types"
 	"github.com/stretchr/testify/suite"
+	"time"
 )
+
+type ChannelsResponse struct {
+	Channels []struct {
+		State        string `json:"state"`
+		Ordering     string `json:"ordering"`
+		Counterparty struct {
+			PortID    string `json:"port_id"`
+			ChannelID string `json:"channel_id"`
+		} `json:"counterparty"`
+		ConnectionsHops []string `json:"connection_hops"`
+		Version         string   `json:"version"`
+		PortID          string   `json:"port_id"`
+		ChannelID       string   `json:"channel_id"`
+	}
+}
+
+type NextSequenceResponse struct {
+	NextSequenceRecv string   `json:"next_sequence_receive"`
+	Proof            []string `json:"proof"`
+	ProofHeight      struct {
+		RevisionNumber string `json:"revision_number"`
+		RevisionHeight string `json:"revision_height"`
+	} `json:"proof_height"`
+}
 
 type BTCTimestampingPhase2TestSuite struct {
 	suite.Suite
@@ -94,6 +120,33 @@ func (s *BTCTimestampingPhase2TestSuite) Test1IbcCheckpointingPhase2() {
 	}
 
 	chainB := s.configurer.GetChainConfig(1)
-	_, err = chainB.GetDefaultNode()
+	validatorNode, err := chainB.GetDefaultNode()
 	s.NoError(err)
+
+	bz, err := validatorNode.QueryGRPCGateway("/ibc/core/channel/v1/channels", nil)
+	s.NoError(err)
+	s.T().Logf("channels: %s", bz)
+	var channelsResponse ChannelsResponse
+	err = json.Unmarshal(bz, &channelsResponse)
+	s.NoError(err)
+
+	// Validate channel state and kind
+	s.Equal("STATE_OPEN", channelsResponse.Channels[1].State)
+	s.Equal("ORDER_ORDERED", channelsResponse.Channels[1].Ordering)
+	s.Contains("wasm", channelsResponse.Channels[1].PortID)
+	// Define channel and port ids
+	channelID := channelsResponse.Channels[1].ChannelID
+	portID := channelsResponse.Channels[1].PortID
+
+	s.T().Log("Sleeping for 10 seconds to allow for IBC packets to be sent")
+	time.Sleep(10 * time.Second)
+
+	// Query next sequence id
+	bz, err = validatorNode.QueryGRPCGateway(fmt.Sprintf("/ibc/core/channel/v1/channels/%s/ports/%s/next_sequence", channelID, portID), nil)
+	s.NoError(err)
+	var nextSequenceResponse NextSequenceResponse
+	err = json.Unmarshal(bz, &nextSequenceResponse)
+	s.NoError(err)
+	// Check that three IBC packets have been received
+	s.Equal("4", nextSequenceResponse.NextSequenceRecv)
 }
