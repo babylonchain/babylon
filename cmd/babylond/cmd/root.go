fa8e667b0e51f25ea78e7027279370249c6edf3e
KonradStaniec <konrad.staniec@gmail.com>
2024-01-30 16:34:45 +0100

    magic fix for autocli opts

--- cmd/babylond/cmd/root.go
+++ cmd/babylond/cmd/root.go
@@ -119,6 +119,10 @@ func NewRootCmd() *cobra.Command {
 	initClientCtx, _ = config.ReadFromClientConfig(initClientCtx)
 	autoCliOpts.Keyring, _ = keyring.NewAutoCLIKeyring(initClientCtx.Keyring)
 	autoCliOpts.ClientCtx = initClientCtx
+	autoCliOpts.TxConfigOpts = tx.ConfigOptions{
+		EnabledSignModes:           tx.DefaultSignModes,
+		TextualCoinMetadataQueryFn: authtxconfig.NewGRPCCoinMetadataQueryFn(initClientCtx),
+	}
 
 	if err := autoCliOpts.EnhanceRootCommand(rootCmd); err != nil {
 		panic(err)
